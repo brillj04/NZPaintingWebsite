68274e5c393599a48373f0a3a053b0777633bede
brillj04 <brillj04@myunitec.ac.nz>
2017-06-15 05:07:44 +1200

    Create MainJS.js

--- MainJS.js
+++ MainJS.js
@@ -1,3 +1,28 @@
+function loadXMLDoc() {
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      myFunction(this);
+    }
+  };
+  xmlhttp.open("GET", "ProductsPaintings.xml", true);
+  xmlhttp.send();
+}
+function myFunction(xml) {
+  var i;
+  var xmlDoc = xml.responseXML;
+  var table="<tr><th>TITLE</th><th>CATEGORY</th></tr>";
+  var x = xmlDoc.getElementsByTagName("PAINTING");
+  for (i = 0; i <x.length; i++) { 
+    table += "<tr><td>" +
+    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+    "</td><td>" +
+    x[i].getElementsByTagName("CATEGORY")[0].childNodes[0].nodeValue +
+    "</td></tr>";
+  }
+  document.getElementById("demo").innerHTML = table;
+}
+
 
 /*=============Membership form validation==============================*/
 function submitForm() {
@@ -21,3 +46,4 @@ function submitForm() {
 	} 
    
 }
+
