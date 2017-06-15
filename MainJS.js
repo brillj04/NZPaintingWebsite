c3f97ba044affefaa5f739e3ea29f3b1a552124a
bulats02 <bulats02@myunitec.ac.nz>
2017-06-16 08:50:09 +1200

    Update MainJS.js

--- MainJS.js
+++ MainJS.js
@@ -1,21 +1,18 @@
 
-/*function myFunction(xml) {
-  var i;
-  var xmlDoc = xml.responseXML;
-  var table="<tr><th>TITLE</th><th>CATEGORY</th></tr>";
-  var x = xmlDoc.getElementsByTagName("PAINTING");
-  for (i = 0; i <x.length; i++) { 
-    table += "<tr><td>" +
-    x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
-    "</td><td>" +
-    x[i].getElementsByTagName("IMAGE")[0].childNodes[0].nodeValue +
-    "</td></tr>";
-  }
-  document.getElementById("demo").innerHTML = table;
-}*/
+var myIndex = 0;
+carousel();
 
-
-
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+       x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 2000); // Change image every 2 seconds
+}
 
 
 /*=============Membership form validation==============================*/
